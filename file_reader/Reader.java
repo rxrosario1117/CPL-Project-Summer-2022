791f999deaabd5da0f10a54836c299a23830477d
rxrosario1117 <99367990+rxrosario1117@users.noreply.github.com>
2022-06-26 21:18:03 -0400

    Added a try catch for readAFile

--- file_reader/Reader.java
+++ file_reader/Reader.java
@@ -8,21 +8,31 @@ public class Reader {
 
 //    String fileText;
 
-    public String readAFile(String fileName) throws IOException {
+    public String readAFile(String fileName) {
 
 //        fileText = "";
 //        fileText = new String(Files.readAllBytes(Paths.get(fileName)));
 
-        return new String(Files.readAllBytes(Paths.get(fileName)));
+        try{
+
+            return new String(Files.readAllBytes(Paths.get(fileName)));
+
+        }catch (IOException e) {
+
+            System.out.println("File: " + fileName + " not found");
+
+        }
+
+        return "";
 
     }
 
 //    Test readAFile
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Reader reader = new Reader();
 
-        String file = reader.readAFile("test2.txt");
+        String file = reader.readAFile("test.txt");
 
         System.out.println(file);
 
